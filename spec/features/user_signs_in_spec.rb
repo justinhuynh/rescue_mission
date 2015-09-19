0a91d5cf8e2d27fbdef3fdb122904f22b217e1a4
justinhuynh <jhuynh@gmail.com>
2015-09-18 22:00:56 -0400

    prep for authentication

---
+++ spec/features/user_signs_in_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+feature 'user signs in', %(
+  As a user
+  I want to sign in
+  So that my questions and answers can be associated to me
+
+  Acceptance Criteria
+  - [ ] I must be able to sign in using either GitHub, Twitter, or Facebook (choose
+    one)
+) do
+
+  scenario 'user successfully signs in' do
+
+  end
+
+  scenario 'user unsuccessfully signs in' do
+  
+  end
+
+end
